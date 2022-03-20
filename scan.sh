c694b9f3428df89223dfb2cb8fd0af847728e82b
Ted <tedmathewdelacruz@gmail.com>
2022-03-20 15:22:37 +0800

    Track scan history

--- scan.sh
+++ scan.sh
@@ -17,7 +17,13 @@ cat $TARGET_DIR/subdomains.txt | sort -u -o $TARGET_DIR/subdomains.txt
 
 cat $TARGET_DIR/subdomains.txt | anew $TARGET_DIR/.tmp/subdomains.txt > $TARGET_DIR/.tmp/new.txt
 
+if [ ! -d "$TARGET_DIR/.history" ]; then
+    mkdir "$TARGET_DIR/.history"
+fi;
+
 if [ -s "$TARGET_DIR/.tmp/new.txt" ];then
+    now=$(date +%s)
+    cp "$TARGET_DIR/.tmp/new.txt" "$TARGET_DIR/.history/$now"
     NUM_SUBDOMAINS=$(wc -l "$TARGET_DIR/.tmp/new.txt" | awk '{print $1}')
     echo "Found $NUM_SUBDOMAINS new subdomains for target: **$TARGET**" | notify -id general
 fi;
